968dae0da632f1057db7ac9b227a6f56d34d9187
George Papadakis <phaistonian@gmail.com>
2017-03-12 19:20:32 +0200

    misc

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const path = require('path');
+const loaderUtils = require('loader-utils');
 
+/* eslint-disable func-names */
 module.exports = function (input) {
-  const options = JSON.parse(this.query.slice(1));
-  const absFile = path.resolve(process.cwd(), options.file);
+  const options = loaderUtils.getOptions(this);
 
-  // Same file
-  if (absFile === this.resourcePath) {
+  // no options
+  if (!input) {
     return input;
   }
 
-  const result = `@import '${absFile}';
+  let files = options.file || options.file;
+
+  if (!files) {
+    return input;
+  }
+
+  if (typeof files === 'string') {
+    files = [files];
+  }
+
+  const absFiles = files
+    .map(f => `${process.cwd()}/${f}`)
+    .filter(f => f !== this.resourcePath);
+
+  const imports = absFiles.map(f => `@import '${f}';`).join('\n');
+  const result = `${imports}
 
 ${input}`;
 
